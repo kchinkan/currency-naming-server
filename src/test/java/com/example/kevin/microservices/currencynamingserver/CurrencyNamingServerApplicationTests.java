6ae982b59f0c9029ee93dfce4b6ed10c1f13b191
Kevin Chinkan <kchinkan@gmail.com>
2024-02-05 17:54:04 -0500

    setup naming server and loadbalancer eureka

---
+++ src/test/java/com/example/kevin/microservices/currencynamingserver/CurrencyNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kevin.microservices.currencynamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
